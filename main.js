0863ec4611dd2a30b39479b1d5679f727f6f5f76
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-05-12 00:32:01 +0600

    learned pin

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.to(".parent-three h1", {
   transform: "translate(-140%)",
   scrollTrigger: {
-    trigger: ".parent-three h1",
+    trigger: ".parent-three ",
     scroller: "body",
     markers: true,
+    start: "top 0%", // যখন trigger এর top, viewport এর 80% জায়গায় আসে
+    end: "top -100%",
+    scrub: 2,
+    pin: true,
   },
 });
